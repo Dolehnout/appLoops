fef23b3cda4bf4435c518937cbebb42d1db126d6
Dolehnout <dolehnout2002@gmail.com>
2022-11-15 21:37:55 +0100

    tablas de multiplicar

---
+++ appLoop/frmTabla.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appLoop
+{
+    public partial class frmTabla : Form
+    {
+        public frmTabla()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnResultado_Click(object sender, EventArgs e)
+        {
+            int tabla = Int32.Parse(this.txtTabla.Text);
+            string aux = "";
+            for (int i = 0; i <= 12; i++)
+            {
+                aux += $"{tabla} x {i} = {tabla * i} \r\n";
+                aux += "";
+                this.txtResultado.Text = aux;
+            }
+        }
+    }
+}
